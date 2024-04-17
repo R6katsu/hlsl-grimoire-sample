681a1490124e561f71d9f29a741a7eeaf6c0cfdd
232e013 <232e013@GAME2023PC09>
2024-04-17 17:30:07 +0900

    R60417変更

--- Sample_02_01/Sample_02_01/Assets/shader/sample.fx
+++ Sample_02_01/Sample_02_01/Assets/shader/sample.fx
@@ -18,16 +18,29 @@ VSOutput VSMain(VSInput In)
     VSOutput vsOut = (VSOutput)0;
 
     // step-1 入力された頂点座標を出力データに代入する
-
+    vsOut.pos = In.pos;
     // step-2 入力された頂点座標を2倍に拡大する
-
+    //vsOut.pos.xyzw *= 2.0f;
     // step-3 入力されたX座標を1.5倍、Y座標を0.5倍にして出力
-
+    vsOut.pos.x *= 1.5f;
+    vsOut.pos.y *= 0.5f;
     return vsOut;
 }
 
-// ピクセルシェーダー
+/// <summary>
+/// ピクセルシェーダー
+/// 色の変更
+/// </summary>
 float4 PSMain(VSOutput vsOut) : SV_Target0
 {
-    return float4(1.0f, 0.0f, 0.0f, 1.0f);
+    //return float4(1.0f, 0.0f, 0.0f, 1.0f); //赤
+    //return float4(1.0f, 1.0f, 1.0f, 1.0f); //白
+    //return float4(0.0f, 0.0f, 0.0f, 1.0f); //黒
+    //return float4(0.5f, 0.5f, 0.5f, 1.0f); //灰
+    return float4(0.25f, 0.25f, 0.25f, 1.0f); //濃灰
+
+    //vsOut.pos.x = 0.5f;
+    //vsOut.pos.y = 0.5f;
+    //vsOut.pos.z = 0.5f;
+    //return float4(vsOut.pos.x, vsOut.pos.y, vsOut.pos.z, 1.0f); //座標によって色を変える
 }
--- Sample_02_02/Sample_02_02/main.cpp
+++ Sample_02_02/Sample_02_02/main.cpp
@@ -41,16 +41,20 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
     // 頂点配列を定義
     SimpleVertex vertices[] = {
         {
-            {-0.5f, -0.5f, 0.0f},
-            { 1.0f, 0.0f, 0.0f }
+            { -0.5f, -0.5f, 0.0f}, //左下
+            { 1.0f, 1.0f, 0.0f }
         },
         {
-            { 0.0f, 0.5f, 0.0f },
-            { 0.0f, 1.0f, 0.0f }
+            { 0.5f, -0.5f, 0.0f }, //右下
+            { 0.0f, 1.0f, 1.0f }
         },
         {
-            { 0.5f, -0.5f, 0.0f },
-            { 0.0f, 0.0f, 1.0f }
+            { -0.5f, 0.5f, 0.0f}, //左上
+            { 1.0f, 0.0f, 1.0f }
+        },
+        {
+            { 0.5f, 0.5f, 0.0f}, //右上
+            { 1.0f, 1.0f, 1.0f }
         }
     };
 
@@ -61,7 +65,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
     // 5. 三角形のインデックスバッファを作成
     //インデックス配列
     uint16_t indices[] = {
-        0,1,2
+        0,3,2,0,3,1
     };
     IndexBuffer triangleIB;
     triangleIB.Init(sizeof(indices), 2);
@@ -93,7 +97,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
         // 5. インデックスバッファを設定
         renderContext.SetIndexBuffer(triangleIB);
         // 6. ドローコール
-        renderContext.DrawIndexed(3);
+        renderContext.DrawIndexed(6);
 
         /// //////////////////////////////////////
         // 絵を描くコードを書くのはここまで！！！
